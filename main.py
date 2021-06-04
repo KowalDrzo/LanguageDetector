ab2663fa2dc142257977485c380b724a2c30fd57
KowalDrzo <248959@student.pwr.edu.pl>
2021-06-04 10:03:02 +0200

    Działa dla 3 języków

--- main.py
+++ main.py
@@ -34,21 +34,56 @@ train_inputs = np.array([eng_data, ger_data, pol_data])
 
 network = NeuralNetwork(26)
 
-print(network.weights)
+#print(network.weights)
 
 train_outputs = np.array([[0, 0, 1]]).T
 
-train_iterations = 50000
+train_iterations = 80000
 
 network.train(train_inputs, train_outputs, train_iterations)
 
-print(network.weights)
+#print(network.weights)
 
 input_text = input("Podaj słowo lub zdanie: ")
 #test_data = tp.parse_string(input_text)
 
 #print(f"Result for {test_data} is:")
 #print(round(network.propagation(np.array(test_data)), 3))
+
+test_data = np.array( [tp.parse_string(input_text)] )
+
+for data in test_data:
+    print(f"Result for {data} is:")
+    print(round(network.propagation(data)[-1], 3))
+
+
+train_inputs = np.array([eng_data, ger_data, pol_data])
+
+network = NeuralNetwork(26)
+
+train_outputs = np.array([[1, 0, 0]]).T
+
+train_iterations = 80000
+
+network.train(train_inputs, train_outputs, train_iterations)
+
+test_data = np.array( [tp.parse_string(input_text)] )
+
+for data in test_data:
+    print(f"Result for {data} is:")
+    print(round(network.propagation(data)[-1], 3))
+
+
+
+train_inputs = np.array([eng_data, ger_data, pol_data])
+
+network = NeuralNetwork(26)
+
+train_outputs = np.array([[0, 1, 0]]).T
+
+train_iterations = 80000
+
+network.train(train_inputs, train_outputs, train_iterations)
 
 test_data = np.array( [tp.parse_string(input_text)] )
 
